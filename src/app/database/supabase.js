7e1d0f9dd2dde8777895ec86fcf5dd80b936c1fd
Matheus Almeida <mathzalmeida@gmail.com>
2024-01-06 17:00:46 -0300

    feat: supabase connection

---
+++ src/app/database/supabase.js
@@ -0,0 +1,8 @@
+const supabaseClient = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
